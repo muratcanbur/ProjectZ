3217641bfa2a1827fa1a30e213ac890ed341d49a
muratcan.bur <6nz5--M2>
2021-04-02 21:15:25 +0300

    create AuthFragment UI.

--- app/src/main/java/co/icanteach/apps/android/projectz/home/HomeFragment.kt
+++ app/src/main/java/co/icanteach/apps/android/projectz/home/HomeFragment.kt
@@ -1,24 +1,23 @@
-package co.icanteach.apps.android.projectz
+package co.icanteach.apps.android.projectz.home
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.navigation.fragment.findNavController
+import co.icanteach.apps.android.projectz.R
 
 /**
- * A simple [Fragment] subclass as the default destination in the navigation.
+ * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class HomeFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
